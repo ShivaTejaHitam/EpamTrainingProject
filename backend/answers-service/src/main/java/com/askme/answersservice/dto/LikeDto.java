913decffd2d2428dcf47d54ea1a2681a5fe93ce6
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2023-10-21 00:45:49 +0530

    Update LikeDto.java

--- backend/answers-service/src/main/java/com/askme/answersservice/dto/LikeDto.java
+++ backend/answers-service/src/main/java/com/askme/answersservice/dto/LikeDto.java
@@ -1,21 +1,33 @@
 package com.askme.answersservice.dto;
 
 public class LikeDto {
-	
+
 	private Long likeId;
 	private Long answerId;
 	private String userEmail;
-	
+
+	public Long getLikeId() {
+		return likeId;
+	}
+
+	public void setLikeId(Long likeId) {
+		this.likeId = likeId;
+	}
+
 	public Long getAnswerId() {
 		return answerId;
 	}
+
 	public void setAnswerId(Long answerId) {
 		this.answerId = answerId;
 	}
+
 	public String getUserEmail() {
 		return userEmail;
 	}
+
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
 	}
+
 }
